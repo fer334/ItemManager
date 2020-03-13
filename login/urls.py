ced747be4aa1798671f29cfecde0748d54bbc6dd
Matias Lopez <matigls2@gmail.com>
2020-03-13 00:51:51 -0300

    se vuelve a poner archivo eliminado por error en algun otro commit

---
+++ login/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from . import views
+app_name = 'PaoApp'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.login, name='login'),
+    path('testLogin/', views.testLogin, name='testLogin'),
+    path('register/', views.register, name='register'),
+    path('postRegister/', views.postRegister, name='postRegister'),
+]
