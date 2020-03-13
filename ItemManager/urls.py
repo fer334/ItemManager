448c4c76f79ccebe9588e535c39c4f6ef234e334
Matias Lopez <matigls2@gmail.com>
2020-03-13 00:18:46 -0300

    actualizado desarrollo

--- ItemManager/urls.py
+++ ItemManager/urls.py
@@ -1,6 +1,7 @@
-"""untitled URL Configuration
+"""ItemManager URL Configuration
+
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('login.urls'))
 ]
--- login/admin.py
+++ login/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-<<<<<<< HEAD
 
-from .models import Usuario
 # Register your models here.
-admin.site.register( Usuario )
-=======
-from .models import Usuario
-# Register your models here.
-admin.site.register(Usuario)
->>>>>>> c06dd248bbc65b9d2c50319399c98cbd5b3a5cef
