6f88f9c6a00b708780790be77142f5b2e92270cf
David <nundavid99@gmail.com>
2020-03-13 17:11:08 -0300

    Cambios en templates para reflejar nueva app de login

--- login/urls.py
+++ login/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-app_name = 'PaoApp'
+app_name = 'login'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
