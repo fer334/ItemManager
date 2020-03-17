7c1ad352e0fd6d79777aae9af45f8ba3ac3971ca
David <nundavid99@gmail.com>
2020-03-16 21:10:28 -0300

    Se agrega modelos de TipoItem, Plantilla de atributos y placeholder de proyecto

--- administracion/models.py
+++ administracion/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Proyecto(models):
+    pass
+
+"""
+Esta clase representa los tipos de items
+"""
+class TipoItem(models):
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField( max_length=800)
+    prefijo = models.CharField(max_length=5)
+    proyecto =  models.ForeignKey(Proyecto, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
+
+
+class PlantillaAtributo(models):
+    nombre = models.CharField(max_length=200)
+    tipo = models.CharField(max_length=100)
+    tipo_item = models.ForeignKey(TipoItem, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
