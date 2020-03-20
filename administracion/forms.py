de198abe84d3aa92428ca3a60adb6fe9a7b2f5af
Matias Lopez <matigls2@gmail.com>
2020-03-20 01:16:47 -0300

    cambio en el template de crearProyecto.html utilizando forms

--- administracion/forms.py
+++ administracion/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.utils import timezone
+from login.models import usr
 
 
 class ProyectoForm(forms.Form):
@@ -9,10 +10,10 @@ class ProyectoForm(forms.Form):
                                    widget=forms.TextInput(attrs={'placeholder': 'Ej. 2020-09-28'}))
     numero_fases = forms.IntegerField(label='Numero de fases del proyecto:', min_value=1,
                                       widget=forms.TextInput(attrs={'placeholder': 'Ej. 7'}))
-    gerente = forms.CharField(label='Gerente del proyecto', max_length=200,
-                              widget=forms.TextInput(attrs={'placeholder': 'Ej. Pedro Parques'}))
     cant_comite = forms.IntegerField(label='Cantidad De Miembros del Comité', min_value=3,
                                      widget=forms.TextInput(attrs={'placeholder': '(obs: nro. impar >=3) Ej. 5'}))
+    eleccion = [(x.localId, x.username) for x in usr.objects.all()]
+    gerente = forms.ChoiceField(label='Gerente del proyecto', initial='Seleccione al Gerente...', choices=eleccion)
 
     def clean_cant_comite(self):
         cant_comite = self.cleaned_data['cant_comite']
