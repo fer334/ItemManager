9d19e2813e9cd2fd721d273b44f6a56f1b33ad6b
David <nundavid99@gmail.com>
2020-03-25 23:57:10 -0400

    Cambios finales en settings para soportar static files

--- ItemManager/settings.py
+++ ItemManager/settings.py
@@ -136,11 +136,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-
+    os.path.join(BASE_DIR, "staticDir"),
 ]
 LOGIN_URL = '/login/'
 AUTH_USER_MODEL = 'login.Usuario'
