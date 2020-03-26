18b55ac4ee88af4d4790b3a3f3291286d1c24508
David <nundavid99@gmail.com>
2020-03-25 23:16:12 -0400

    Cambios en settings para soportar static files

--- ItemManager/urls.py
+++ ItemManager/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('login.urls')),
     path('', include('administracion.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
