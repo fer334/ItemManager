c3b081fd43c90c76f3148a1c0d389b960672557a
Matias Lopez <matigls2@gmail.com>
2020-04-06 14:51:19 -0400

    Se implementa los modelos de ítem y atributo particular

--- desarrollo/models.py
+++ desarrollo/models.py
@@ -1,3 +1,28 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+
 # Create your models here.
+
+class Item(models.Model):
+    nombre = models.CharField(max_length=200, null=False)
+    estado = models.CharField(max_length=100, null=False)
+    version = models.CharField(null=False, default='1.0.0')
+    complejidad = models.PositiveIntegerField(default=5, null=False,
+                                              validators=[MinValueValidator(1), MaxValueValidator(10)])
+    descripcion = models.CharField(max_length=200, null=True)
+    tipo_item = models.ForeignKey('administracion.TipoItem', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
+
+class AtributoParticular(models.Model):
+    item = models.ManyToManyField('desarrollo.Item')
+    nombre = models.CharField(max_length=200, null=False)
+    tipo = models.CharField(max_length=100, null=False)
+    valor = models.CharField(max_length=300, null=False)
+    es_requerido = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nombre
