f635c0411adbbce337fef9b11c77879ad85ed688
fer <luis.fer334@live.com>
2020-04-13 09:45:12 -0400

    Mejoras en la documentacion

--- docs.bash
+++ docs.bash
@@ -2,4 +2,5 @@ cd docs
 rm -rvf source/_autosummary
 make html
 cd build/html
-firefox index.html
+nohup xdg-open index.html > /dev/null
+echo "Hace scroll hacia arriba y arregla todo los warnigs(las lineas de color rojooo)"
