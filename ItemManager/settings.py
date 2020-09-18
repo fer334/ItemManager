c14e19abdc3933cd1d3ca12d9c186275fae0743b
fer <luis.fer334@live.com>
2020-09-18 13:59:30 -0400

    static files for desarrollo

--- ItemManager/settings.py
+++ ItemManager/settings.py
@@ -137,10 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
+    os.path.join(BASE_DIR, "static"),
 ]
 LOGIN_URL = '/login/'
 AUTH_USER_MODEL = 'login.Usuario'
