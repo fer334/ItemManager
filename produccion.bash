1ff89aca8639edc3857d2b53ae5a17f87a58f3dc
fer <luis.fer334@live.com>
2020-10-05 20:13:37 -0300

    fix script.bash

--- produccion.bash
+++ produccion.bash
@@ -3,11 +3,20 @@
 
 branch=$1
 backupfile=$2
+tag=$3
+
+echo "Clonando el repositorio"
+git clone https://github.com/fer334/ItemManager/ || { echo "Error al clonar el repositorio" ; exit; }
+
+echo "Ingresando al proyecto"
+cd ItemManager || { echo "El directorio ItemManager no existe" ; exit; }
+
+git checkout $tag
 
 echo "Loggeando en heroku"
 heroku login
 echo "Realizando el push a la rama de heroku"
-git push heroku $branch
+git push heroku $branch:$tag
 echo "Poblando la base de datos"
 heroku pg:psql < $backupfile
 
